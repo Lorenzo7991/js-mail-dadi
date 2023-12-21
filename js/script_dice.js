cd956f4e478b661c38d1ce0f1ba39378bd732f69
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2023-12-21 18:17:03 +0100

    add console first dice function

--- js/script_dice.js
+++ js/script_dice.js
@@ -1,2 +1,23 @@
 /* DOM - JS sheet test response */
 console.log('JS OK');
+
+/* Initialize dice throw variables and assign randomize value */
+const playerOne = Math.floor(Math.random() * 6) + 1;
+const playerTwo = Math.floor(Math.random() * 6) + 1;
+/* variables value checking */
+console.log(`Giocatore 1: (${playerOne})   Giocatore 2: (${playerTwo})`); 
+
+/* Initialize result support variable */
+let result = '';
+
+/* Establish winner */
+if(playerOne > playerTwo){
+    result = "Ha vinto il giocatore 1!"
+} else if (playerOne < playerTwo){
+    result = "Ha vinto il giocatore 2!"
+} else {
+    result = "E' un pareggio! lancia di nuovo."
+}
+
+/* Printing result variable */
+console.log(result);
