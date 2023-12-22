acb22e2e702dca5920b08bb282ed95e42aed0259
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2023-12-22 01:59:57 +0100

    add draft DOM layout & user email verify printed on DOM

--- js/script_login.js
+++ js/script_login.js
@@ -1,28 +1,38 @@
-/* DOM - JS sheet test response */
 console.log('JS OK');
 
+/* Adding event listener on button element */
+document.getElementById('btn-verify').addEventListener('click', function () {
+    /* Assigning email DOM element as value of the emailElement variable */
+    const emailElement = document.getElementById('email');
 
-/* Initializing and assigning array variable as registered emails */
-const emailList = ["fake_email1@emailfake.it", "fake_email2@emailfake.it", "fake_email3@emailfake.it","fake_email4@emailfake.it", "fake_email5@emailfake.it", "lorenzo1997chierisini@gmail.com"  ];
-/* array value check */
-console.table(emailList);
+   
+    /* Recovery value of emailElement variable and assigning as value of userEmail variable */
+    const userEmail = emailElement.value;
+    /* Variable userEmail value check */
+    console.log("Valore attuale di userEmail:" + " " + userEmail);
+    
+    /* Initializing and assigning array variable as registered emails */
+    const emailList = ["fake_email1@emailfake.it", "fake_email2@emailfake.it", "fake_email3@emailfake.it", "fake_email4@emailfake.it", "fake_email5@emailfake.it", "lorenzo1997chierisini@gmail.com"];
+    /*Variable array value check */
+    console.table(emailList);
 
-/*Initializing and assigning variable as user email request */
-const userEmail = prompt("Inserisci la tua email");
+    /* Initializing boolean variable for tracking user email presence */
+    let emailFound = false;
 
-/* Initializing boolean variable for tracking usere email prensence */
-let emailFound = false;
-
-
-/* Checking user email presence */
-for (let i = 0; i < emailList.length && emailFound === false; i++) {
-    if (emailList[i] === userEmail) {
-        emailFound = true;
+    /* Checking user email presence */
+    for (let i = 0; i < emailList.length && emailFound === false; i++) {
+        if (emailList[i] === userEmail) {
+            emailFound = true;
+        }
     }
-}
 
-if(emailFound){
-    console.log("Bentornato!");
-} else {
-    console.log("Mi dispiace, la tua email non risulta tra quelle registrate");
-}
+
+     /* Initializing resultMessage variable and assigning DOM class as value  */
+    const resultMessage = document.querySelector('.result-message');
+    /* Verrifying emailFound value and assigning matching value of resultMessage variable  */
+    if (emailFound) {
+        resultMessage.innerText = "La tua email è verificata, bentornato!";
+    } else {
+        resultMessage.innerText = "Mi dispiace, la tua email non risulta tra quelle registrate...";
+    }
+});
